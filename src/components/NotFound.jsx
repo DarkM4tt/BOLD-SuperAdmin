d4dfbebdc6c1070db45525c1311676776bc71080
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2025-04-01 02:41:42 +0530

    Updated folder structure

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex items-center justify-center font-redhat text-4xl font-bold text-red-400">
+      Page not found!
+    </div>
+  );
+};
+
+export default NotFound;
