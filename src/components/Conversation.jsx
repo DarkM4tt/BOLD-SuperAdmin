7ef353da461ea95699052b945f4008d3098f2b9d
Prabhat Mani Srivastava <prabhatmani933@gmail.com>
2025-04-09 01:29:53 +0530

    Fixed bugs

--- src/components/Conversation.jsx
+++ src/components/Conversation.jsx
@@ -5,7 +5,7 @@ import { useFetchRideDetailsQuery } from "../features/rideApi";
 import { useFetchChatsQuery } from "../features/chatApi";
 import { format } from "date-fns";
 import LoadingAnimation from "./common/LoadingAnimation";
-import BackArrow from "../assets/BackArrow.svg";
+import BackArrow from "../assets/backArrow.svg";
 
 const Conversation = () => {
   const params = useParams();
@@ -97,81 +97,86 @@ const Conversation = () => {
           Customer / Driver chat history
         </p>
       </div>
-
-      {rideData?.driver_info?.full_name &&
-        rideData?.customer_info?.full_name && (
+      {results?.length > 0 ? (
+        <>
           <p className="ml-10 text-[#777777] font-redhat font-normal text-base">
             This conversation is only between {rideData?.driver_info?.full_name}{" "}
             & {rideData?.customer_info?.full_name}
           </p>
-        )}
 
-      <Paper
-        sx={{
-          width: "100%",
-          height: "600px",
-          overflowY: "auto",
-          padding: 2,
-          display: "flex",
-          flexDirection: "column",
-          gap: 1,
-          borderRadius: "8px",
-          marginTop: "20px",
-          backgroundColor: "#E8E8E8",
-          boxShadow: "none",
-        }}
-        ref={chatContainerRef}
-      >
-        {allMessages.map((msg, idx) => {
-          const isDriverMsg = isDriver(msg.sender);
-          const isCustomerMsg = isCustomer(msg.sender);
-          const alignRight = isCustomerMsg || (!isDriverMsg && !isCustomerMsg);
+          <Paper
+            sx={{
+              width: "100%",
+              height: "600px",
+              overflowY: "auto",
+              padding: 2,
+              display: "flex",
+              flexDirection: "column",
+              gap: 1,
+              borderRadius: "8px",
+              marginTop: "20px",
+              backgroundColor: "#E8E8E8",
+              boxShadow: "none",
+            }}
+            ref={chatContainerRef}
+          >
+            {allMessages?.map((msg, idx) => {
+              const isDriverMsg = isDriver(msg.sender);
+              const isCustomerMsg = isCustomer(msg.sender);
+              const alignRight =
+                isCustomerMsg || (!isDriverMsg && !isCustomerMsg);
 
-          return (
-            <Box
-              key={idx}
-              sx={{
-                display: "flex",
-                justifyContent: alignRight ? "flex-end" : "flex-start",
-              }}
-            >
-              <Box
-                sx={{
-                  maxWidth: "65%",
-                  backgroundColor: alignRight ? "#e0e0e0" : "#18C4B8",
-                  color: alignRight ? "#000" : "#fff",
-                  px: 2,
-                  py: 1,
-                  borderRadius: "12px",
-                  wordWrap: "break-word",
-                }}
-              >
-                <Typography sx={{ fontSize: "18px", fontWeight: 600 }}>
-                  {msg.text}
-                </Typography>
-                <Typography
-                  variant="caption"
+              return (
+                <Box
+                  key={idx}
                   sx={{
-                    display: "block",
-                    textAlign: "right",
-                    fontSize: "0.6rem",
-                    opacity: 0.6,
-                    mt: 0.5,
+                    display: "flex",
+                    justifyContent: alignRight ? "flex-end" : "flex-start",
                   }}
                 >
-                  {format(new Date(msg?.send_at), "dd MMM, yyyy h:mm a")}
-                </Typography>
-              </Box>
-            </Box>
-          );
-        })}
+                  <Box
+                    sx={{
+                      maxWidth: "65%",
+                      backgroundColor: alignRight ? "#e0e0e0" : "#18C4B8",
+                      color: alignRight ? "#000" : "#fff",
+                      px: 2,
+                      py: 1,
+                      borderRadius: "12px",
+                      wordWrap: "break-word",
+                    }}
+                  >
+                    <Typography sx={{ fontSize: "18px", fontWeight: 600 }}>
+                      {msg.text}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      sx={{
+                        display: "block",
+                        textAlign: "right",
+                        fontSize: "0.6rem",
+                        opacity: 0.6,
+                        mt: 0.5,
+                      }}
+                    >
+                      {format(new Date(msg?.send_at), "dd MMM, yyyy h:mm a")}
+                    </Typography>
+                  </Box>
+                </Box>
+              );
+            })}
 
-        {(isFetching || loading) && (
-          <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
-            <CircularProgress size={20} />
-          </Box>
-        )}
-      </Paper>
+            {(isFetching || loading) && (
+              <Box sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
+                <CircularProgress size={20} />
+              </Box>
+            )}
+          </Paper>
+        </>
+      ) : (
+        <p className="text-lg font-redhat font-bold text-red-400">
+          No chats for this ride!
+        </p>
+      )}
     </>
   );
 };
